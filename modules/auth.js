f89297f7f6d9827d4fc21c0cf8066b4409b2d001
Joprocorp <joprocorp@gmail.com>
2022-02-17 15:58:51 +0100

    Changing header used for authentification from 'Authorization' to 'Token' due to issues with Swift implementation

--- modules/auth.js
+++ modules/auth.js
@@ -9,7 +9,7 @@ const {decrypt} = require("./crypto");
 module.exports = {
     is_auth: function(req, callback){
         if(req.headers && req.headers["Token"]){
-            let token = req.headers.authorization.split(' ')
+            let token = req.headers["Token"].split(' ')
             if(token.length > 1 && token[0] === "Bearer"){
                 token = token[1]
                 db.select('SELECT * FROM users u JOIN action_links al ON u.user_id == al.user_id JOIN session_tokens st ON st.user_id = u.user_id WHERE st.session_token = ? AND al.type = \'email\' AND al.activated = 1 AND u.blocked = 0', [token], function(rows){
